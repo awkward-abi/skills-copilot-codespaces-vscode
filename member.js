62b93fe1a201bf3000abe20a8a0273757e7dda27
Gail Larupay <79824839+awkward-abi@users.noreply.github.com>
2023-08-03 09:44:48 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMembes() {
+    return {
+        restrict: 'E',
+        templateUrl: 'app/components/member/skills.html',
+        controller: 'SkillsController',
+        controllerAs: 'skillsCtrl'
+    };
+}
